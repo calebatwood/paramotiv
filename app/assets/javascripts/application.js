2f2c69edc00940e55b69eb6aa687bca68633c6a7
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-05-27 12:53:58 -0600

    Year dropdown populates when user selects a car make/model

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,13 +14,13 @@ $(document).ready(function(){
         var makeArray = data.makes;
         console.log(data.makes);
         $.each(makeArray, function(index, value) {
-          $('#car_make').append('<option value="' + value.name + '">' + value.name + '</option>');
+          $('#car_make').append('<option value="' + value.niceName + '">' + value.name + '</option>');
         });
       }
   });
   //display car model selection
   $(document).on('change', '#car_make', function() {
-  retrieve_model(this.value);
+    retrieve_model(this.value);
   });
 
   function retrieve_model(make) {
@@ -29,10 +29,26 @@ $(document).ready(function(){
       console.log(data.models);
       $('#car_model').empty();
       $.each(modelArray, function(index, value) {
-        $('#car_model').append('<option>' + value.name + '</option>');
+        $('#car_model').append('<option value="' + value.niceName + '">' + value.name + '</option>');
       });
     });
   }
+  //display car model year selection
+  $(document).on('change', '#car_model', function(){
+    var make = $('#car_make').val();
+    retrieve_years(make, this.value);
+  });
 
+  function retrieve_years(make, model) {
+    $.get('https://api.edmunds.com/api/vehicle/v2/:'+make+'/:'+model+'/years?fmt=json&api_key=scgz9esm95u72e7rh8mv5kyz', function(data) {
+      var yearsArray = data.years;
+      console.log(data.years);
+      console.log('hi');
+      $('car_year').empty();
+      $.each(yearsArray, function(index, value) {
+        $('#car_year').append('<option>' + value.year + '</option>');
+      });
+    });
+  }
 
 });
